f636d41985c913ad8fdd7a37b0c81068e615ef91
DIT <d7703_21@d7703-21ui-imac.local>
2018-04-19 11:13:58 +0900

    윤

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func bt(_ sender: Any) {
+        
+        //오버로딩(한 객체에서 2개의 인자사용) = 중복정의
+        //오버라이딩(델리게이트) = 재정의
+        let myAlertController = UIAlertController(title: "아을람", message: "시이간이 되었슴", preferredStyle: .alert)
+        
+        //alertAction 만들기
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: {
+            (myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.blue
+            
+        })
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: {(myAction: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.white
+            
+        })
+        
+        let testAction = UIAlertAction(title: "삭제", style: .destructive, handler: nil)
+        
+        
+        
+        
+        //AlectAction을 AlectController
+        myAlertController.addAction(okAction)
+        myAlertController.addAction(cancelAction)
+        myAlertController.addAction(testAction)
+        
+        //화면에 출력
+        present(myAlertController, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
